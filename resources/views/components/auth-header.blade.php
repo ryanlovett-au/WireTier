7cd60d4d15f2226bd3218bc529bf9d7dbfe88795
Ryan Lovett <ryan.lovett@skerric.com.au>
2026-03-29 01:16:46 +1030

    Add app name to auth pages header

--- resources/views/components/auth-header.blade.php
+++ resources/views/components/auth-header.blade.php
@@ -4,6 +4,7 @@
 ])
 
 <div class="flex w-full flex-col text-center">
+    <flux:heading size="xl" class="mb-1">{{ config('app.name') }}</flux:heading>
     <flux:heading size="xl">{{ $title }}</flux:heading>
     <flux:subheading>{{ $description }}</flux:subheading>
 </div>
